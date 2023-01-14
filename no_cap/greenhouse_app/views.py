874836a87e6d960d8207cffab76e24e7dcdd4554
ic <64978735+YugioCard59@users.noreply.github.com>
2023-01-13 21:59:24 -0600

    fixing redirects related to deleting

--- no_cap/greenhouse_app/views.py
+++ no_cap/greenhouse_app/views.py
@@ -32,10 +32,6 @@ def login_home(request):
         getHash = request.POST['writeToDom']
         print(f"from login view writetodom: {getHash}")
         request.session['getHash'] = getHash
-        # new_token_form = Token_storage(token_value=getHash, token_user=request.user)
-        # print(f"This is token form: {new_token_form}")
-        # new_token_form.save()
-
         dir = './media/random_app/'
         for f in os.listdir(dir):
             os.remove(os.path.join(dir, f))
@@ -45,14 +41,11 @@ def login_home(request):
         if isFile:
             os.remove(path)
         return render(request, "greenhouse_app/signin.html", {"getHash": getHash})
-        # return redirect("greenhouse_app:signin")
+    
     return render(request, "greenhouse_app/login_home.html")
 
 @login_required
 def show_seedling(request):
-    # path = "./static/cleanedList.json"
-    # isFile = os.path.isfile(path)
-    # if isFile:
     dir = './media/random_app/'
     for f in os.listdir(dir):
         os.remove(os.path.join(dir, f))
@@ -62,39 +55,35 @@ def show_seedling(request):
     if isFile:
         os.remove(path)
     if request.method == "GET" and request.user.is_authenticated:
-        # try:
-        seedling_list = Token_storage.objects.filter(token_user=request.user)
-        content = {
-            'seedling_list': seedling_list
-        }
-        getHash = request.session['getHash']
-        for element in seedling_list:
-        # for seedling in seedling_list:
-            if element.token_value == getHash:
-                # print(f"typeof hash: {type(getHash)} typeof seedling: {(element.token_value)}")
-                element.delete()
-                # return seedling_list
-        # context = {
-        #     'getHash': request.POST.get('getHash')
-        # }
-        
-        # getHash = request.POST['writeToDom']
-                print(f"from show_seedling view writetodom: {getHash}")
-                new_token_form = Token_storage(token_value=getHash, token_user=request.user)
-    # print(f"This is token form: {new_token_form}")
-                new_token_form.save()
-
-
-        # return redirect('random_app:handle_csv')
-        # except:
-        #     seedling_list = Token_storage.objects.filter(token_user=request.user)
-        #     content = {
-        #         'seedling_list': seedling_list
-        #     }
+        try:
+            seedling_list = Token_storage.objects.filter(token_user=request.user)
+            content = {
+                'seedling_list': seedling_list
+            }
+            getHash = request.session['getHash']
+            for element in seedling_list:
+                if element.token_value == getHash:
+                    element.delete()
+            
+                    print(f"from show_seedling view writetodom: {getHash}")
+                    new_token_form = Token_storage(token_value=getHash, token_user=request.user)
+                    new_token_form.save()
+   
+        except:
+            seedling_list = Token_storage.objects.filter(token_user=request.user)
+            content = {
+                'seedling_list': seedling_list
+            }
+            return render(request, "greenhouse_app/show_seedling.html", content)
+     
         return render(request, "greenhouse_app/show_seedling.html", content)
 
 
 def delete_seedling(request):
+    seedling_list = Token_storage.objects.filter(token_user=request.user)
+    content = {
+        'seedling_list': seedling_list
+        }
     if request.method == 'POST':
         delete_list = request.POST.getlist("seedDelete")
         print(delete_list)
@@ -103,15 +92,8 @@ def delete_seedling(request):
             print(seedling_id)
             print(Token_storage.objects.get(pk=int(seedling_id)))
             Token_storage.objects.get(pk=int(seedling_id)).delete()
-
-    seedling_list = Token_storage.objects.filter(token_user=request.user)
-    content = {
-        'seedling_list': seedling_list
-        }
-        
+  
     return render(request, "greenhouse_app/show_seedling.html", content)
-    # return redirect("greenhouse_app:show_seedling")
-
 
 def signup(request):
     if request.method == "POST":
@@ -151,23 +133,6 @@ def signin(request):
         return redirect('greenhouse_app:login_home')
         # WILL NOT LET ME USE NAVIGATION TO RETURN TO RANDOMAPP:UPLOAD
 
-    # elif request.user.is_authenticated and request.method == "POST":
-    #     getHash = request.POST['writeToDom']
-    #     print(f"from signin view writetodom: {getHash}")
-    #     new_token_form = Token_storage(token_value=getHash, token_user=request.user)
-    #     # print(f"This is token form: {new_token_form}")
-    #     new_token_form.save()
-
-    #     dir = './media/random_app/'
-    #     for f in os.listdir(dir):
-    #         os.remove(os.path.join(dir, f))
-    #     path = "./static/cleanedList.json"
-    #     isFile = os.path.isfile(path)
-    #     print(f"from signin view does json exist: {isFile}")
-    #     if isFile:
-    #         os.remove(path)
-    #     return redirect('greenhouse_app:show_seedling')
-    
     elif request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -175,31 +140,6 @@ def signin(request):
         if user is not None:
             login(request, user)
             username = user.username
-            # return redirect('random_app:handle_csv')
-            # dir = './media/random_app/'
-            # for f in os.listdir(dir):
-            #     os.remove(os.path.join(dir, f))
-            # path = "./static/cleanedList.json"
-            # isFile = os.path.isfile(path)
-            # print(isFile)
-            # if isFile:
-            #     os.remove(path)
-        #     if request.method == "POST" and request.user.is_authenticated:
-        #         getHash = request.POST['writeToDom']
-        #         print(getHash)
-        #         new_token_form = Token_storage(token_value=getHash, token_user=request.user)
-        # # print(f"This is token form: {new_token_form}")
-        #         new_token_form.save()
-
-        #         dir = './media/random_app/'
-        #         for f in os.listdir(dir):
-        #             os.remove(os.path.join(dir, f))
-        #         path = "./static/cleanedList.json"
-        #         isFile = os.path.isfile(path)
-        #         print(isFile)
-        #         if isFile:
-        #             os.remove(path)
-                # return redirect('greenhouse_app:show_seedling')
             return render(request, "greenhouse_app/login_home.html", {'username': username})
         else:
             messages.error(request, "Bad Credentials")
